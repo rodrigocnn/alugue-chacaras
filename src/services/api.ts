d15cf5f695e04b5a04baaadffe2a503f6198ddbe
Rodrigo Cesar <rodrigocnn@gmail.com>
2024-10-16 13:46:12 -0300

    chore: add service file

---
+++ src/services/api.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+});
+
+const apis = {
+  index: (endpoint: string) => instance.get(endpoint),
+  store: (endpoint: string, data: any) => instance.post(endpoint, data),
+  show: (endpoint: string, id: string) => instance.get(`/${endpoint}/${id}`),
+  update: (endpoint: string, id: string, data: object) =>
+    instance.put(`/${endpoint}/${id}`, data),
+  delete: (endpoint: string, id: string) =>
+    instance.delete(`/${endpoint}/${id}`),
+  login: (endpoint: string, data: any) => instance.post(endpoint, data),
+};
+
+export default apis;
