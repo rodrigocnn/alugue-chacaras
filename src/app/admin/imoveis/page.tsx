ebe7f7bb1d823bf54d4ef7f13537f12588011042
Rodrigo Cesar <rodrigocnn@gmail.com>
2024-10-21 13:46:09 -0300

    feat: add buton create  page Imoveis

--- src/app/admin/imoveis/page.tsx
+++ src/app/admin/imoveis/page.tsx
@@ -4,12 +4,14 @@ import Layout from '@/components/custom/layout';
 import { DataGrid } from '@/components/custom/datagrid';
 import { columns } from '@/modules/imoveis/columns';
 import { useGetImoveis } from '@/modules/imoveis/hooks/useGetImoveis';
+import { Button } from '@/components/ui/button';
 
 export default function Imoveis() {
   const { queryImoveis } = useGetImoveis();
 
   return (
     <Layout>
+      <Button type="submit">Novo Imóvel</Button>
       <DataGrid data={queryImoveis.data || []} columns={columns} />
     </Layout>
   );
