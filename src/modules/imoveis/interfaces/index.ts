f7a380b7a5c2d3565ec2568efdd42b4668b8c234
Rodrigo Cesar <rodrigocnn@gmail.com>
2024-10-21 11:12:39 -0300

    feat: add interface User

--- src/modules/imoveis/interfaces/index.ts
+++ src/modules/imoveis/interfaces/index.ts
@@ -2,8 +2,22 @@ export interface Imovel {
   id?: string;
   name: string;
   description: string;
-  locationUrl: string;
-  city: string;
+  location: string;
+  googleMapsUrl: string;
+  cityId: string;
   imgFeature: string;
-  imgUrl: string;
+}
+
+export interface InputImovel extends Imovel {
+  imagemDestaque: File;
+}
+
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+  role: string;
+  contact: string;
+  whatsapp?: string | null;
 }
